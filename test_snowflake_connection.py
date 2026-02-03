cc825d6f5a3d5b0a670ce5e4ed8137a193298b44
Sree Teja Reddy Chappidi <csreetejareddy@gmail.com>
2026-02-03 11:54:59 -0500

    Update Snowflake connection parameters

--- test_snowflake_connection.py
+++ test_snowflake_connection.py
@@ -1,15 +1,16 @@
 from snowflake.snowpark import Session
 
 connection_parameters = {
-    "account": "onnqpxp-ovc21017.us-east-1",
+    "account": "onnqpxp-ovc21017111.us-east-1",
     "user": "SREETEJA0908",
-    "password": "Sreetejareddy@0908",
+    "password": "enteryours",
     "role": "ACCOUNTADMIN",
     "warehouse": "COMPUTE_WH",
-    "database": "USER$SREETEJA0908",
+    "database": "USER$SREEenterurs",
     "schema": "ANALYTICS"
 }
 
 session = Session.builder.configs(connection_parameters).create()
 print("✅ Connected successfully")
 session.close()
+
